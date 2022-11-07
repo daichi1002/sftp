7dcc82aa4422612d28844ec32b45090059da1984
daichinagano <d.nagano1002@gmail.com>
2022-11-07 14:35:26 +0900

    ✨ add create sales data

--- service/save_sales_data.go
+++ service/save_sales_data.go
@@ -4,5 +4,9 @@ import "sftp/domain"
 
 func (s Service) saveSalesData(data []*domain.SalesData) {
 	// 売上データの保存
+	err := s.salesDataRepository.CreaateSalesData(data)
 
+	if err != nil {
+		s.logger.Fatalf("Failed to create sales data : %v", err)
+	}
 }
